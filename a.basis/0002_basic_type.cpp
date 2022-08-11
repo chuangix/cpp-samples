d8a99b842e0db22cafefcbbf9a309e7745900248
chuangix <chuangix@outlook.com>
2022-08-11 22:59:33 +0800

    feat: 0002_basic_type.cpp

---
+++ a.basis/0002_basic_type.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << __FILE__ << endl;
+    char c;
+    uint8_t u8;
+    uint16_t u16;
+    uint32_t u32;
+    uint64_t u64;
+    int8_t i8;
+    int16_t i16;
+    int32_t i32;
+    int64_t i64;
+    float f;
+    double d;
+    cout << "The size of \"char\":" << sizeof(c) << endl;
+    cout << "The size of \"uint8_t\":" << sizeof(u8) << endl;
+    cout << "The size of \"uint16_t\":" << sizeof(u16) << endl;
+    cout << "The size of \"uint32_t\":" << sizeof(u32) << endl;
+    cout << "The size of \"uint64_t\":" << sizeof(u64) << endl;
+    cout << "The size of \"int8_t\":" << sizeof(i8) << endl;
+    cout << "The size of \"int16_t\":" << sizeof(i16) << endl;
+    cout << "The size of \"int32_t\":" << sizeof(i32) << endl;
+    cout << "The size of \"int64_t\":" << sizeof(i64) << endl;
+    return 0;
+}
